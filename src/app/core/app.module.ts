cb10438216efd4e9a608fa69d268832a587c6db5
Elijah Lopez <Elijah.kane.1972@gmail.com>
2022-08-04 21:30:12 -0700

    Day 1 Summer Work

---
+++ src/app/core/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from '../services/routing/app-routing.module';
+import { AppComponent } from '../components/app.component';
+import { StoreModule } from '@ngrx/store';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    StoreModule.forRoot({}, {})
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,108 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./src/**/*.{html,js}'],
   purge: [],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'media', // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Poppins', 'sans-serif'],
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'brand': {
+        'main': {
+          DEFAULT: '#F8AD9D',
+          'muted': 'rgba(248, 173, 157, 0.14)',
+        },
+        'secondary': {
+          DEFAULT: '#51727B',
+          'muted': 'rgba(81, 114, 123, 0.14)',
+        },
+        'accent': {
+          DEFAULT: '#F4978E',
+          'muted': 'rgba(244,151,142,0.14)',
+        },
+        'shade': {
+          DEFAULT: '#F08080',
+          'muted': 'rgba(240,128,128,0.14)',
+        },
+        'background': {
+          DEFAULT: '#F5F5F5',
+          'muted': 'rgba(245,245,245,0.14)',
+        },
+      },
+      'utility': {
+        'primary': {
+          DEFAULT: '#2B478C',
+          'muted': 'rgba(43,71,140,0.14)',
+        },
+        'secondary': {
+          DEFAULT: '#5F585A',
+          'muted': 'rgba(95,88,90,0.14)',
+        },
+        'danger': {
+          DEFAULT: '#EB5E55',
+          'muted': 'rgba(235,94,85,0.14)',
+        },
+        'info': {
+          DEFAULT: '#6C91C2',
+          'muted': 'rgba(108,145,194,0.14)',
+        },
+        'warning': {
+          DEFAULT: '#FEB95F',
+          'muted': 'rgba(254,185,95,0.14)',
+        },
+        'success': {
+          DEFAULT: '#5C9962',
+          'muted': 'rgba(92,153,98,0.14)',
+        },
+      },
+      'rainbow': {
+        'red': {
+          DEFAULT: '#EB5E55',
+          'muted': 'rgba(235,94,85,0.14)',
+        },
+        'orange': {
+          DEFAULT: '#F58C5A',
+          'muted': 'rgba(245,140,90,0.14)',
+        },
+        'yellow-orange': {
+          DEFAULT: '#FAA35D',
+          'muted': 'rgba(250,163,93,0.14)',
+        },
+        'yellow': {
+          DEFAULT: '#FEB95F',
+          'muted': 'rgba(254,185,95,0.14)',
+        },
+        'yellow-green': {
+          DEFAULT: '#BFB672',
+          'muted': 'rgba(191,182,114,0.14)',
+        },
+        'green': {
+          DEFAULT: '#5C9962',
+          'muted': 'rgba(92,153,98,0.14)',
+        },
+        'light-blue': {
+          DEFAULT: '#6C91C2',
+          'muted': 'rgba(108,145,194,0.14)',
+        },
+        'blue': {
+          DEFAULT: '#2B478C',
+          'muted': 'rgba(43,71,140,0.14)',
+        },
+        'purple': {
+          DEFAULT: '#565290',
+          'muted': 'rgba(86,82,144,0.14)',
+        },
+        'lilac': {
+          DEFAULT: '#805D93',
+          'muted': 'rgba(128,93,147,0.14)',
+        },
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
