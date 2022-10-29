61682770001e158648017ac7d25b1de10f54b358
Elijah Lopez <elijah.kane.1972@gmail.com>
2022-10-29 13:55:03 -1000

    Small Change

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -1,59 +1,48 @@
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { faBook, faCalendars, faClock, faFilePen, faObjectsColumn, faSchool } from '@fortawesome/pro-duotone-svg-icons';
-import {
-   Event,
-   NavigationCancel,
-   NavigationEnd,
-   NavigationError,
-   NavigationStart,
-   Router,
-   RouterOutlet
-} from '@angular/router';
+import { Apollo, gql } from 'apollo-angular';
+import { Event, NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router } from '@angular/router';
 
 @Component({
-   selector: 'app-root',
-   templateUrl: './app.component.html',
-   styleUrls: ['./app.component.css'],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit, AfterViewInit {
-   sideNavIcons = {
-      dashboard: faObjectsColumn,
-      calendar: faCalendars,
-      schedule: faClock,
-      homework: faBook,
-      notes: faFilePen,
-      courses: faSchool,
-   };
-   loading: boolean;
-   @ViewChild(RouterOutlet) routerOutlet: RouterOutlet;
+export class AppComponent implements OnInit {
+  sideNavIcons = {
+    dashboard: faObjectsColumn,
+    calendar: faCalendars,
+    schedule: faClock,
+    homework: faBook,
+    notes: faFilePen,
+    courses: faSchool,
+  };
+  loading: boolean;
 
-   constructor(private router: Router) {
-   }
+  constructor(private router: Router, private apollo: Apollo) {
+  }
 
-   ngOnInit() {
-      this.router.events.subscribe((event: Event) => {
-         switch (true) {
-            case event instanceof NavigationStart:
-            {
-               this.loading = true;
-               break;
-            }
-            case event instanceof NavigationEnd:
-            case event instanceof NavigationCancel:
-            case event instanceof NavigationError:
-            {
-               this.loading = false;
-               break;
-            }
-            default:
-            {
-               break;
-            }
-         }
-      });
-   }
-
-   ngAfterViewInit() {
-   }
+  ngOnInit() {
+    this.router.events.subscribe((event: Event) => {
+      switch (true) {
+        case event instanceof NavigationStart:
+        {
+          this.loading = true;
+          break;
+        }
+        case event instanceof NavigationEnd:
+        case event instanceof NavigationCancel:
+        case event instanceof NavigationError:
+        {
+          this.loading = false;
+          break;
+        }
+        default:
+        {
+          break;
+        }
+      }
+    });
+  }
 }
 
